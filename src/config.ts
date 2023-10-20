0f7ce5c567202564ba0a7cfb037f6da1890e936e
Etienne Donneger <23462475+Krow10@users.noreply.github.com>
2023-10-20 10:13:43 -0400

    Fix default table name to match Clickhouse default (#25)
    * Fix default table name to match Clickhouse default
    
    The Clickhouse sink tool creates a table `Clock` for storing data
    related to block numbers and timestamp. Use that as default value for
    table name parameter (renamed to env `DB_TABLE` for avoiding conflict
    with Clickhouse sink).
    
    See: https://github.com/pinax-network/substreams-sink-clickhouse
    
    * Fix `bun-test` workflow

--- src/config.ts
+++ src/config.ts
@@ -7,7 +7,7 @@ import pkg from "../package.json";
 export const DEFAULT_PORT = "8080";
 export const DEFAULT_HOSTNAME = "localhost";
 export const DEFAULT_DB_HOST = "http://localhost:8123";
-export const DEFAULT_DB_NAME = "demo";
+export const DEFAULT_DB_TABLE = "Clock";
 export const DEFAULT_DB_USERNAME = "default";
 export const DEFAULT_DB_PASSWORD = "";
 export const DEFAULT_MAX_ELEMENTS_QUERIES = 10;
@@ -18,7 +18,7 @@ const CommanderSchema = z.object({
     port: z.string().default(DEFAULT_PORT),
     hostname: z.string().default(DEFAULT_HOSTNAME),
     dbHost: z.string().default(DEFAULT_DB_HOST),
-    name: z.string().default(DEFAULT_DB_NAME),
+    name: z.string().default(DEFAULT_DB_TABLE),
     username: z.string().default(DEFAULT_DB_USERNAME),
     password: z.string().default(DEFAULT_DB_PASSWORD),
     maxElementsQueried: z.coerce.number().gte(2).default(DEFAULT_MAX_ELEMENTS_QUERIES).describe(
@@ -38,7 +38,7 @@ const opts = new Command()
     .addOption(new Option("--port <int>", "Server listen on HTTP port").default(DEFAULT_PORT).env("PORT"))
     .addOption(new Option("--hostname <string>", "Server listen on HTTP hostname").default(DEFAULT_HOSTNAME).env("HOST"))
     .addOption(new Option("--db-host <string>", "Clickhouse DB HTTP hostname").default(DEFAULT_DB_HOST).env("DB_HOST"))
-    .addOption(new Option("--name <string>", "Clickhouse DB table name").default(DEFAULT_DB_NAME).env("DB_NAME"))
+    .addOption(new Option("--name <string>", "Clickhouse DB table name").default(DEFAULT_DB_TABLE).env("DB_TABLE"))
     .addOption(new Option("--username <string>", "Clickhouse DB username").default(DEFAULT_DB_USERNAME).env("DB_USERNAME"))
     .addOption(new Option("--password <string>", "Clickhouse DB password").default(DEFAULT_DB_PASSWORD).env("DB_PASSWORD"))
     .addOption(new Option("--max-elements-queried <string>",

b97145d8e8832a4083c74205ed1577f166453242
Denis Carriere <carriere.denis@gmail.com>
2023-10-21 23:03:22 -0400

    add tests for queries

--- src/fetch/block.ts
+++ src/fetch/block.ts
@@ -1,5 +1,6 @@
-import { config } from "../config.js";
-import { getBlock } from "../queries.js";
+import { makeQuery } from "../clickhouse/makeQuery.js";
+import { logger } from "../logger.js";
+import { Block, getBlock } from "../queries.js";
 
 export default async function (req: Request) {
   const { searchParams } = new URL(req.url);
@@ -7,14 +8,14 @@ export default async function (req: Request) {
   const block_number = searchParams.get("block_number");
   const block_id = searchParams.get("block_id");
   const timestamp = searchParams.get("timestamp");
-  let limit = parseInt(searchParams.get("limit") ?? "500");
-  const params: any = { chain, block_number, block_id, timestamp, limit };
-  if ( limit > config.maxElementsQueried ) limit = config.maxElementsQueried;
+  const limit = searchParams.get("limit");
 
   // TO-DO: Max block number
-  // TO-DO: sort by DESC/ASC (default DESC)
   try {
-    const response = await getBlock(params);
+    const params = { chain, block_number, block_id, timestamp, limit };
+    const query = await getBlock(params);
+    const response = await makeQuery<Block>(query)
+    logger.info('getBlock', {params, query});
     return new Response(JSON.stringify(response.data), { headers: { "Content-Type": "application/json" } });
   } catch (e: any) {
     return new Response(e.message, { status: 400 });
--- src/queries.ts
+++ src/queries.ts
@@ -1,18 +1,34 @@
 import { config } from './config';
 import { makeQuery } from './clickhouse/makeQuery';
-import { logger } from './logger';
 
-export async function getBlock(options: {block_id?: string, block_number?: number, timestamp?: string, chain?: string, limit: number}) {
-    let query = `SELECT block_number, block_id, timestamp, chain FROM ${config.table}`;
+export interface Block {
+    block_number: number;
+    block_id: string;
+    timestamp: string;
+    chain: string;
+}
+export function parseLimit(limit?: string|null|number) {
+    let value = 1; // default 1
+    if (limit) {
+        if (typeof limit === "string") value = parseInt(limit);
+        if (typeof limit === "number") value = limit;
+    }
+    // limit to maxElementsQueried
+    if ( value > config.maxElementsQueried ) value = config.maxElementsQueried;
+    return value;
+}
+
+export function getBlock(options: {block_id?: string|null, block_number?: string|null, timestamp?: string|null, chain?: string|null, limit?: string|null}) {
+    const limit = parseLimit(options.limit);
+    let query = `SELECT * FROM ${config.table}`;
     const where = [];
     if ( options.chain ) where.push(`chain == '${options.chain}'`);
     if ( options.block_id ) where.push(`block_id == '${options.block_id}'`);
     if ( options.block_number ) where.push(`block_number == '${options.block_number}'`);
     if ( where.length ) query += ` WHERE (${where.join(' AND ')})`;
     query += ' ORDER BY block_number DESC'
-    query += ` LIMIT ${options.limit}`
-    logger.info('getBlock', options)
-    return makeQuery<{chain: string}>(query);
+    query += ` LIMIT ${limit}`
+    return query;
 }
 
 export async function supportedChainsQuery() {

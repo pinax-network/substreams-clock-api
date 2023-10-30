2d73ce26088eea793caaf3036fcfa27255f792c9
Denis Carriere <carriere.denis@gmail.com>
2023-10-30 19:08:43 -0400

    add cors

--- index.ts
+++ index.ts
@@ -1,7 +1,9 @@
 import { config } from "./src/config.js";
 import { logger } from "./src/logger.js";
 import GET from "./src/fetch/GET.js";
+import OPTIONS from "./src/fetch/OPTIONS.js";
 import * as prometheus from "./src/prometheus.js";
+import { BadRequest } from "./src/fetch/cors.js";
 
 if (config.verbose) logger.enable();
 
@@ -10,8 +12,9 @@ const app = Bun.serve({
     port: config.port,
     fetch(req: Request) {
         if (req.method === "GET") return GET(req);
+        if (req.method === "OPTIONS") return OPTIONS(req);
         prometheus.request_error.inc({pathname: new URL(req.url).pathname, status: 400});
-        return new Response("Invalid request", { status: 400 });
+        return BadRequest
     }
 });
 
--- src/fetch/block.ts
+++ src/fetch/block.ts
@@ -2,6 +2,7 @@ import { makeQuery } from "../clickhouse/makeQuery.js";
 import { logger } from "../logger.js";
 import { Block, getBlock } from "../queries.js";
 import * as prometheus from "../prometheus.js";
+import { BadRequest, toJSON } from "./cors.js";
 
 export default async function (req: Request) {
   try {
@@ -9,10 +10,10 @@ export default async function (req: Request) {
     logger.info({searchParams: Object.fromEntries(Array.from(searchParams))});
     const query = await getBlock(searchParams);
     const response = await makeQuery<Block>(query)
-    return new Response(JSON.stringify(response.data), { headers: { "Content-Type": "application/json" } });
+    return toJSON(response.data);
   } catch (e: any) {
     logger.error(e);
     prometheus.request_error.inc({pathname: "/block", status: 400});
-    return new Response(e.message, { status: 400 });
+    return BadRequest
   }
 }

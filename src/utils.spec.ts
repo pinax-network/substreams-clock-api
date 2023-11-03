f755963ae7359dd2c002c1c4f4df39ff81e70e9a
Etienne Donneger <23462475+Krow10@users.noreply.github.com>
2023-11-03 09:00:25 -0400

    Add more input type validation to prevent injection (#49)
    SQL injection was possible on most string types, although it's impact
    was limited due to the client connection being `readonly`. The changes
    add more robust input validation using a combination of `zod` and type
    casts.

--- src/utils.spec.ts
+++ src/utils.spec.ts
@@ -1,20 +1,48 @@
 import { expect, test } from "bun:test";
-import { parseBlockId, parseLimit, parseTimestamp } from "./utils.js";
-import { DEFAULT_MAX_LIMIT } from "./config.js";
+import { parseBlockId, parseBlockNumber, parseChain, parseLimit, parseSortBy, parseTimestamp } from "./utils.js";
+import { DEFAULT_MAX_LIMIT, DEFAULT_SORT_BY } from "./config.js";
 
 test("parseBlockId", () => {
     expect(parseBlockId()).toBeUndefined();
     expect(parseBlockId(null)).toBeUndefined();
+    expect(parseBlockId("invalid")).toBeUndefined();
     expect(parseBlockId("00fef8cf2a2c73266f7c0b71fb5762f9a36419e51a7c05b0e82f9e3bacb859bc")).toBe("00fef8cf2a2c73266f7c0b71fb5762f9a36419e51a7c05b0e82f9e3bacb859bc");
     expect(parseBlockId("0x00fef8cf2a2c73266f7c0b71fb5762f9a36419e51a7c05b0e82f9e3bacb859bc")).toBe("00fef8cf2a2c73266f7c0b71fb5762f9a36419e51a7c05b0e82f9e3bacb859bc");
+});
+
+test("parseBlockNumber", () => {
+    expect(parseBlockNumber()).toBeUndefined();
+    expect(parseBlockNumber(null)).toBeUndefined();
+    expect(parseBlockNumber(-1)).toBeUndefined();
+    expect(parseLimit("invalid")).toBeNaN();
+    expect(parseBlockNumber("10")).toBe(10);
+    expect(parseBlockNumber(10)).toBe(10);
+});
+
+test("parseChain", () => {
+    expect(parseChain()).toBeUndefined();
+    expect(parseChain(null)).toBeUndefined();
+    expect(parseChain("' OR 1=1)--")).toBeUndefined();
+    expect(parseChain("test")).toBe("test");
+    expect(parseChain("test123")).toBe("test123");
 });
 
 test("parseLimit", () => {
     expect(parseLimit()).toBe(1);
     expect(parseLimit(null)).toBe(1);
+    expect(parseLimit(-1)).toBe(1);
+    expect(parseLimit("invalid")).toBeNaN();
     expect(parseLimit("10")).toBe(10);
     expect(parseLimit(10)).toBe(10);
     expect(parseLimit(999999)).toBe(DEFAULT_MAX_LIMIT);
+});
+
+test("parseSortBy", () => {
+    expect(parseSortBy()).toBe(DEFAULT_SORT_BY);
+    expect(parseSortBy(null)).toBe(DEFAULT_SORT_BY);
+    expect(parseSortBy("invalid")).toBe(DEFAULT_SORT_BY);
+    expect(parseSortBy("ASC")).toBe("ASC");
+    expect(parseSortBy("DESC")).toBe("DESC");
 });
 
 test("parseTimestamp", () => {
@@ -37,5 +65,3 @@ test("parseTimestamp", () => {
     expect(() => parseTimestamp(10)).toThrow("Invalid timestamp");
     expect(() => parseTimestamp("10")).toThrow("Invalid timestamp");
 });
-
-

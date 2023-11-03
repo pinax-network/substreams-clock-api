b1990b02450a9fa8efd9a7e5a401273a414f47dc
Krow10 <23462475+Krow10@users.noreply.github.com>
2023-11-03 12:23:45 -0400

    Remove `chains` import for `getBlock` query
    The import of the `chains` variable from `fetch/openapi.ts` caused
    a DB query to be executed once the module was loaded. This behavior
    was also preventing testing any function inside the `queries.ts` module.
    
    The fix makes the `getBlock` function explicitly query the DB for
    supported chains (as they might also get updated over time). The
    function is mocked in the tests using the latest Bun feature (1.0.8+)
    of *mock modules* in order to allow "offline" testing.

--- src/queries.spec.ts
+++ src/queries.spec.ts
@@ -1,17 +1,26 @@
-import { expect, test } from "bun:test";
-import { getBlock, getChain } from "./queries.js";
-import { chains } from './fetch/openapi.js';
+import { expect, jest, mock, test } from "bun:test";
+import { createBlockQuery, getBlock, getChain } from "./queries.js";
+import { supportedChainsQuery } from "./fetch/chains.js";
 
-test("getBlock", () => {
-    expect(getBlock(new URLSearchParams({ chain: "eth", block_number: "123" })))
+// Mock supported chains data to prevent DB query
+mock.module("./fetch/chains.ts", () => ({ supportedChainsQuery: jest.fn().mockResolvedValue(["eth", "polygon"]) }));
+
+test("createBlockQuery", () => {
+    expect(createBlockQuery(new URLSearchParams({ chain: "eth", block_number: "123" })))
         .toBe(`SELECT * FROM blocks WHERE (chain == 'eth' AND block_number == '123') ORDER BY block_number DESC LIMIT 1`);
 
-    expect(getBlock(new URLSearchParams({ chain: "eth", greater_or_equals_by_timestamp: '1438270048', less_or_equals_by_timestamp: '1438270083', limit: '3' })))
+    expect(createBlockQuery(new URLSearchParams({ chain: "eth", greater_or_equals_by_timestamp: '1438270048', less_or_equals_by_timestamp: '1438270083', limit: '3' })))
         .toBe(`SELECT * FROM blocks WHERE (toUnixTimestamp(timestamp) >= 1438270048 AND toUnixTimestamp(timestamp) <= 1438270083 AND chain == 'eth') ORDER BY block_number DESC LIMIT 3`);
+});
 
-    // Check that if not chain parameter is passed, all chains are included in the selection
-    chains.forEach((chain) => {
-        expect(getBlock(new URLSearchParams({ block_number: "123" })))
+test("getBlock", async () => {
+    const singleChainQuery = new URLSearchParams({ chain: "eth", block_number: "123" });
+    expect(getBlock(singleChainQuery)).resolves.toBe(createBlockQuery(singleChainQuery));
+
+    // Check that if no chain parameter is passed, all chains are included in the selection
+    let supportedChains = await supportedChainsQuery();
+    supportedChains.forEach((chain) => {
+        expect(getBlock(new URLSearchParams({ block_number: "123" }))).resolves
         .toContain(`SELECT * FROM blocks WHERE (chain == '${chain}' AND block_number == '123') ORDER BY block_number DESC LIMIT 1`);
     });
 });
